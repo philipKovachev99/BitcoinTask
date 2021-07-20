bdd374bb5283ffef34a98cd2e7f91812b3ca02bd
philipKovachev99 <sdfsffsdf642@gmail.com>
2021-07-20 12:27:40 +0300

    added Axios. created a mock json file and displayed the data in a Vuetify table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,64 +1,55 @@
 <template>
-  <v-container>
- 
-  </v-container>
+<div>
+  <v-data-table
+    :headers="headers"
+    :items="bitcoinInfo"
+    :hide-default-footer="true"
+  ></v-data-table>
+</div>
+
+
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import axios from 'axios';
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
+  export default {
+    data () {
+      return {
+        bitcoinInfo: [],
+        headers: [
+          {
+            text: 'Currency',
+            align: 'start',
+            value: 'currency',
+          },
+          
+          { text: '30 Days Ago', value: '30d' },
+          { text: '30 Day Diff %', value: 'thirtyDaysDiff' },
+          { text: '7 Days Ago', value: '7d' },
+          { text: '7 Day Diff %', value: 'sevenDaysDifference' },
+          { text: '24 Hours Ago', value: '24h' },
+        ],
+      }
+    },
+
+    methods: {
+      getBitcoinData() {
+        axios
+      .get('data.json')
+      .then((response => {
+
+      var convertedCollection =  Object.keys(response.data).map(key => {
+            return {currency: key, ...response.data[key]}
+          })
+          this.bitcoinInfo = convertedCollection
+      })) 
         },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+      },
+           
+      mounted() {
+        this.getBitcoinData()
+      },
   }
+
 </script>
